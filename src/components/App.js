dabf1bdc56f40c479833c31cbead9e6a82657d15
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-16 12:06:07 +0530

    app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
-{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
 
 import React from 'react';
-import Review from './Review';
+import Review from './Review.js';
 import './App.css'; 
 
 function App() {
   return (
-    <div className="container">
+    <sectiom className="container">
       <h2 id="review-heading">Our Reviews</h2>
       <main>
         <Review />
       </main>
-    </div>
+    </sectiom>
   );
 }
 
